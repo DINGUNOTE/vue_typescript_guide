5572b617c2c19b79f12c143ba6b2650176585f56
DINGU <web.ljg@gmail.com>
2022-03-18 16:56:39 +0900

    # Vuex 타입 정의 #3

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -13,7 +13,8 @@ import Vue from 'vue';
 import ToolBar from './components/ToolBar.vue';
 import Spinner from './components/SpinnerObject.vue';
 import bus from './utils/bus';
-import { MutationTypes } from './store/mutations';
+// import { MutationTypes } from './store/mutations';
+import { ActionTypes } from './store/actions';
 export default Vue.extend({
   components: {
     ToolBar,
@@ -32,9 +33,10 @@ export default Vue.extend({
       this.loading = false;
     },
   },
-  created() {
-    this.$store.state.news;
-    this.$store.commit(MutationTypes.SET_NEWS);
+  async created() {
+    // this.$store.state.news;
+    // this.$store.commit(MutationTypes.SET_NEWS);
+    await this.$store.dispatch(ActionTypes.FETCH_NEWS);
     bus.$on('on:progress', this.onProgress);
     bus.$on('off:progress', this.offProgress);
   },
--- vue-news/src/store/types.ts
+++ vue-news/src/store/types.ts
@@ -1,4 +1,6 @@
-import { CommitOptions, Store } from 'vuex';
+import { CommitOptions, DispatchOptions, Store } from 'vuex';
+import { Actions } from './actions';
+import { Getters } from './getters';
 import { Mutations } from './mutations';
 import { RootState } from './state';
 
@@ -10,4 +12,24 @@ type MyMutations = {
   ): ReturnType<Mutations[K]>;
 };
 
-export type MyStore = Omit<Store<RootState>, 'commit'> & MyMutations;
+type MyActions = {
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload?: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>;
+};
+
+type MyGetters = {
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>;
+  };
+};
+
+export type MyStore = Omit<
+  Store<RootState>,
+  'commit' | 'dispatch' | 'getters'
+> &
+  MyMutations &
+  MyActions &
+  MyGetters;
